37b3adf60911bee8d11d8d41259f21253596f46c
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-02-27 00:45:57 +0000

    will use pathkit and commander

--- Package.swift
+++ Package.swift
@@ -1,5 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftXCTestLinuxNameGenerator"
+    name: "SwiftXCTestLinuxNameGenerator",
+    dependencies:[
+    	.Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0),
+    	.Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0)
+    ]
 )
